7c02de98ef6224a96ce2dd645e84b7faf5031623
LucasDCoelho <darkalp99@gmail.com>
2022-02-04 14:12:14 -0300

    Aula Jquery e Ajax da DIO

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+    
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP - " + response.cep);
+            $(".cep").show();
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+        }
+    })
+}
+
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide()
+});
+
+
+
